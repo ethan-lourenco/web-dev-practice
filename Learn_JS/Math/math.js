685020033e51399a76b01cdc6672ff2db9f2df12
ethan-lourenco <lourencoethan@yahoo.com>
2025-04-30 22:45:30 -0700

    Update math.js

--- Learn_JS/Math/math.js
+++ Learn_JS/Math/math.js
@@ -59,3 +59,27 @@ console.log(`It's a ${weather} day!`); // It's a sunny day!
    - generates a random floating-point number between 0 (inclusive)
      and 1 (exclusive)
 */
+
+// NaN === NaN - returns false
+// isNaN(value) - checks for NaN using type coercion
+// Number.isNan(value) - checks if it is exactly NaN (no type coercion)
+
+/* parseFloat() && parseInt()
+   - methods that parse a string argument and return
+     a floating-point number or integer respectively
+*/
+
+/* .toFixed()
+   - built-in JS function that formats a number using
+     fixed-point notation
+*/
+
+let numb = 3.14159;
+console.log(numb.toFixed(2));
+// Output: "3.14" (string not number)
+
+/* Switch Statements
+   - evaluates an expression and matches its value against
+     a series of case clauses, when a match is found, the
+     code is executed
+*/
