8fb825691a29654a73f5c444cce5c1b795ef06e4
ethan-lourenco <lourencoethan@yahoo.com>
2025-05-21 20:42:38 -0700

    Create react.js

---
+++ Learn_JS/React/react.js
@@ -0,0 +1,88 @@
+// May 21st 2025
+
+/* JS Libraries and Frameworks
+   - JS libraries and frameworks provide pre-build code that
+     streamlines the development process, while both libraries
+     and frameworks serve to improve productivity and standardize
+     coding practices, they differ in their approach and level of
+     control they provide for developers
+*/
+
+/* JS Libraries 
+   - libraries are generally more focused on providing solutions to 
+     specific tasks, such as manipulating the DOM, handling events,
+     or managing AJAX requests
+*/
+
+/* JS Frameworks 
+   - frameworks provide a more defined structure for building apps,
+     they often come with a set of rules and conventions that
+     developers need to follow
+*/
+
+/* Single Page Applications (SPAs)
+   - unlike traditional multi-page websites, single page apps load 
+     only one HTML page and dynamically update the content as the
+     interacts with the app, without reloading the entire page
+*/
+
+/* React
+   - one of the most popular JS libraries for building user interfaces
+     and web applications
+*/
+
+/* State
+   - refers to the data that determines how a component renders
+     and behaves
+*/
+
+/* React Components
+   - the building blocks of React apps that allow devs to break down
+     complex UI into smaller, manageable pieces, making it easier to
+     develop and maintain large-scale apps, there are Functional and 
+     Class Based Components
+*/
+
+/* JSX
+   - a syntax extension for JS that looks similar to HTML but allows
+     you to write UI elements in a more declarative way
+*/
+
+// React Component Example
+function Greeting() {
+  const name = "John";
+  {
+    /* The result will be Hello John*/
+  }
+  return (
+    <>
+      <h1>Hello {name}</h1>
+      <p>Nice to meet you.</p>
+    </>
+  );
+}
+
+/* To use in app you would write something like this: 
+   <Greeting />
+*/
+
+/* Vite
+   - build tool that aims to provide a faster development experience
+     for modern web projects
+*/
+
+/* Dependencies
+   - libraries or packages that your React project requires 
+     in order to function correctly, such as React itself, 
+     ReactDOM, or other third-party packages that provide
+     additional functionality.
+*/
+
+/* package.json
+   - a key configuration file in
+     projects that contains metadata about your
+     project, including its name, version, and
+     dependencies. It also defines scripts, licensing
+     information, and other settings that help manage
+     the project and its dependencies.
+*/
